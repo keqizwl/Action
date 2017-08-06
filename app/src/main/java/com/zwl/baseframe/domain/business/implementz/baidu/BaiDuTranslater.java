0e05286b7c2fff5f293a6ebc90ade07abad2d9ac
keqizwl <keqibs@163.com>
2017-08-06 15:31:09 +0800

    feat:单词翻译功能

--- app/src/main/java/com/zwl/baseframe/domain/business/implementz/baidu/BaiDuTranslater.java
+++ app/src/main/java/com/zwl/baseframe/domain/business/implementz/baidu/BaiDuTranslater.java
@@ -1,12 +1,14 @@
 package com.zwl.baseframe.domain.business.implementz.baidu;
 
 
+import com.google.gson.internal.LinkedTreeMap;
 import com.zwl.baseframe.domain.business.implementz.util.JsonUtils;
-import com.zwl.baseframe.domain.business.implementz.util.StringUtils;
 import com.zwl.baseframe.domain.business.model.WordModel;
 import com.zwl.baseframe.domain.business.module.Constants;
 import com.zwl.baseframe.domain.business.module.word.IWordModule;
 import com.zwl.baseframe.implementz.di.scope.AppScope;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -32,8 +34,9 @@ public class BaiDuTranslater implements IWordModule {
             public Flowable<WordModel> apply(String wordName) throws Exception {
                 TransApi api = new TransApi(Constants.BAIDU_TRANSLATE_APP_ID, Constants.BAIDU_TRANSLATE_SECURITY_KEY);
                 String result = api.getTransResult(wordName, "auto", "zh");
-                BaiduTransResult baiduTransResult = JsonUtils.fromJson(result);
-                return Flowable.just(new WordModel(0, wordName, StringUtils.unicode2String(baiduTransResult.getTrans_result().get(0).getDst()), System.currentTimeMillis()));
+                LinkedTreeMap<String, Object> transResult = JsonUtils.fromJson(result);
+                ArrayList<LinkedTreeMap<String, String>> trans = (ArrayList<LinkedTreeMap<String, String>>) transResult.get("trans_result");
+                return Flowable.just(new WordModel(0, wordName, trans.get(0).get("dst"), System.currentTimeMillis()));
             }
         };
     }
--- app/src/main/java/com/zwl/baseframe/domain/business/implementz/baidu/BaiduTransResult.java
+++ app/src/main/java/com/zwl/baseframe/domain/business/implementz/baidu/BaiduTransResult.java
@@ -1,60 +1,26 @@
 package com.zwl.baseframe.domain.business.implementz.baidu;
 
-import java.util.List;
-
 /**
- * Created by hasee on 2017/8/5.
+ * Created by hasee on 2017/8/6.
  */
 
 public class BaiduTransResult {
-    //  {"from":"en","to":"zh","trans_result":[{"src":"hello","dst":"\u4f60\u597d"}]}
+    private String src;
+    private String dst;
 
-    private String from;
-    private String to;
-    private List<TransResult> trans_result;
-
-    public String getFrom() {
-        return from;
+    public String getSrc() {
+        return src;
     }
 
-    public void setFrom(String from) {
-        this.from = from;
+    public void setSrc(String src) {
+        this.src = src;
     }
 
-    public String getTo() {
-        return to;
+    public String getDst() {
+        return dst;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public List<TransResult> getTrans_result() {
-        return trans_result;
-    }
-
-    public void setTrans_result(List<TransResult> trans_result) {
-        this.trans_result = trans_result;
-    }
-
-    public static class TransResult{
-        private String src;
-        private String dst;
-
-        public String getSrc() {
-            return src;
-        }
-
-        public void setSrc(String src) {
-            this.src = src;
-        }
-
-        public String getDst() {
-            return dst;
-        }
-
-        public void setDst(String dst) {
-            this.dst = dst;
-        }
+    public void setDst(String dst) {
+        this.dst = dst;
     }
 }
